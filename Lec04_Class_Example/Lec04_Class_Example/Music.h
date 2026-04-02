d26e6c39eb4876ffb093d9d88d1d5685e5cc5310
jonghyun5866 <hyun5866@jbnu.ac.kr>
2026-04-02 15:50:38 +0900

    MusicService added

---
+++ Lec04_Class_Example/Lec04_Class_Example/Music.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+//Music Class
+class Music {
+private:
+	string title;
+	string artist;
+	string album;
+	int year;
+
+public:
+	Music(string title, string artist, string album, int year) {
+		this->title = title;
+		this->artist = artist;
+		this->album = album;
+		this->year = year;
+	}
+
+	string getTitle() { return title; }
+	string getArtist() { return artist; }
+	string getAlbum() { return album; }
+	int getYear() { return year; }
+
+};
+
+//Music Streaming Service Class
+class MusicStreamingService {
+private:
+	string serviceName;
+	vector<Music> musicList;
+
+public:
+	MusicStreamingService(string name) {
+		this->serviceName = name;
+	}
+
+	//음악 추가 기능
+	void addMusic(string title, string artist, string album, int year) {
+		Music newMusic(title, artist, album, year);
+		musicList.push_back(newMusic);
+		cout << "\"" << title << "\" by \"" << artist << "\" is added to " << serviceName << endl;
+	}
+	Music* searchByTitle(string title) {
+		for (int i = 0; i < musicList.size(); i++) {
+			if (musicList[i].getTitle() == title) {
+				return &musicList[i];
+			}
+		}
+		return NULL;
+	}
+
+};
--- Lec04_Class_Example/Lec04_Class_Example/main.cpp
+++ Lec04_Class_Example/Lec04_Class_Example/main.cpp
@@ -1,5 +1,6 @@
 #include "Rectangle.h"
 #include "Pet.h"
+#include "Music.h"
 
 using namespace std;
 int main() {
@@ -21,6 +22,28 @@ int main() {
 	cout << "My puppy is " << myPuppy.getBreed() << endl;
 
 	myPet.infoPet();
+
+	printf("===========================\n");
+
+	//음악 스트리밍 서비스
+	MusicStreamingService myService("Melon");
+	//새로운 음악 추가
+	myService.addMusic("Jump", "Blackpink", "JumpAlbum", 2025);
+	myService.addMusic("Swim", "BTS", "ARIRANG", 2026);
+	myService.addMusic("BANG BANG", "IVE", "REVIVE", 2026);
+	myService.addMusic("404", "KiiKii", "Delulu", 2026);
+
+	//검색 해보기
+	string music_title;
+	cout << "Enter the Music Title: ";
+	cin >> music_title;
+	Music* result = myService.searchByTitle(music_title);
+	if (result != NULL) {
+		cout << "Found: " << result->getTitle() << endl;
+	}
+	else {
+		cout << "not Found" << endl;
+	}
 
 	return 0;
 }
