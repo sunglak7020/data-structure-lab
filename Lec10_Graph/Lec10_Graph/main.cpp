b39ea5702edc06292a3493355870b8a0cfd71aff
jonghyun5866 <hyun5866@jbnu.ac.kr>
2026-05-14 15:32:18 +0900

    DFS added

--- Lec10_Graph/Lec10_Graph/main.cpp
+++ Lec10_Graph/Lec10_Graph/main.cpp
@@ -1,4 +1,5 @@
 #include "AdjMatGraph.h" // 그래프 클래스 포함
+
 int main()
 {
 	AdjMatGraph g; // 새로운 그래프 객체 생성
@@ -16,6 +17,27 @@ int main()
 	fopen_s(&fp, "../text_out.txt", "w");
 	g.display(fp);
 	fclose(fp);
+
+	SrchAMGraph g2; // DFS 탐색기능이 있는 그래프 객체 생성
+	//g.load("graph.txt"); // 파일 "graph.txt"로부터 g를 설정함
+	for (int i = 0; i < 8; i++)
+		g2.insertVertex('A' + i); // 정점 삽입: 'A' 'B', ...
+	g2.insertEdge(0, 1); // 간선 삽입
+	g2.insertEdge(0, 3);
+	g2.insertEdge(1, 2);
+	g2.insertEdge(1, 3);
+	g2.insertEdge(2, 3);
+	g2.insertEdge(2, 3);
+	g2.insertEdge(4, 5);
+	g2.insertEdge(3, 6);
+	g2.insertEdge(5, 7);
+	g2.insertEdge(0, 5);
+	printf("그래프(graph.txt)\n");
+	g2.display(); // 그래프를 화면에 출력
+	printf("DFS ==> ");
+	g2.resetVisited(); // 모든 정점을 방문하지 않았다고 하고
+	g2.DFS(0); // 0번째 정점(A)에서 깊이 우선 탐색 시작
+	printf("\n");
 
 	return 0;
 }
