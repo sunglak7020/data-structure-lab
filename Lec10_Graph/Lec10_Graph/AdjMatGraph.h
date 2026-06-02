5314e30f9a8acf9661c3ab67f3245613845aeac8
jonghyun5866 <hyun5866@jbnu.ac.kr>
2026-06-02 16:22:28 +0900

    maybe final save

--- Lec10_Graph/Lec10_Graph/AdjMatGraph.h
+++ Lec10_Graph/Lec10_Graph/AdjMatGraph.h
@@ -1,7 +1,10 @@
 #pragma once
 #define MAX_VTXS 50
-
+#define INF 9999
+#define MAX_HEAP_NODE 100
 #include <stdio.h>
+#include <iostream>
+using namespace std;
 
 class AdjMatGraph {
 protected:
@@ -60,5 +63,180 @@ public:
 		for (int w = 0; w < size; w++)
 			if (isLinked(v, w) && visited[w] == false)
 				DFS(w); // 연결 + 방문X => 순환호출로 방문
+	}
+};
+
+class VertexSets {
+	int parent[MAX_VTXS]; // 부모 정점의 id
+	int nSets; // 집합의 개수
+public:
+	VertexSets(int n) : nSets(n) {
+		for (int i = 0; i < nSets;i++)
+			parent[i] = -1; // 모든 정점이 고유의 집합에 속함
+	}
+	bool isRoot(int i) { return parent[i] < 0; }
+	int findSet(int v) { // v가 속한 집합을 찾아 반환
+		while (!isRoot(v)) v = parent[v];
+		return v;
+	}
+	void unionSets(int s1, int s2) { // 집합 s1을 집합 s2에 합침
+		parent[s1] = s2;
+		nSets--;
+	}
+};
+
+class WGraph : public AdjMatGraph {
+public:
+	void insertEdge(int u, int v, int weight) {
+		if (weight > INF) weight = INF;
+		setEdge(u, v, weight);
+	}
+	bool hasEdge(int i, int j) { return (getEdge(i, j) < INF); }
+	void load(const char* filename) {
+		FILE* fp;
+		fopen_s(&fp, filename, "r");
+		if (fp != NULL) {
+			int n, val;
+			fscanf_s(fp, "%d", &n);
+			for (int i = 0; i < n; i++) {
+				char str[80];
+				int val;
+				fscanf_s(fp, "%s", str, sizeof(str));
+				insertVertex(str[0]);
+				for (int j = 0; j < n; j++) {
+					fscanf_s(fp, "%d", &val);
+					insertEdge(i, j, val);
+				}
+			}
+		}
+		fclose(fp);
+	}
+
+};
+
+class HeapNode {
+private:
+	int key;    // 간선의 가중치
+	int v1;     // 정점 1
+	int v2;     // 정점 2
+
+public:
+	HeapNode(int k = 0, int u = 0, int v = 0)
+		: key(k), v1(u), v2(v) {
+	}
+
+	void setKey(int k, int u, int v) {
+		key = k;
+		v1 = u;
+		v2 = v;
+	}
+
+	int getKey() const {
+		return key;
+	}
+
+	int getV1() const {
+		return v1;
+	}
+
+	int getV2() const {
+		return v2;
+	}
+};
+
+class MinHeap {
+private:
+	HeapNode node[MAX_HEAP_NODE];
+	int size;
+
+public:
+	MinHeap() : size(0) {
+	}
+
+	bool isEmpty() const {
+		return size == 0;
+	}
+
+	bool isFull() const {
+		return size >= MAX_HEAP_NODE - 1;
+	}
+
+	HeapNode& getParent(int i) {
+		return node[i / 2];
+	}
+
+	void insert(int key, int u, int v) {
+		if (isFull()) {
+			cout << "Heap is full." << endl;
+			return;
+		}
+
+		int i = ++size;
+
+		// 새로 들어온 간선의 가중치가 부모보다 작으면 위로 이동
+		while (i != 1 && key < getParent(i).getKey()) {
+			node[i] = getParent(i);
+			i /= 2;
+		}
+
+		node[i].setKey(key, u, v);
+	}
+
+	HeapNode remove() {
+		if (isEmpty()) {
+			cout << "Heap is empty." << endl;
+			return HeapNode();
+		}
+
+		HeapNode item = node[1];
+		HeapNode last = node[size--];
+
+		int parent = 1;
+		int child = 2;
+
+		while (child <= size) {
+			// 오른쪽 자식이 있고, 오른쪽 자식이 더 작으면 오른쪽 선택
+			if (child < size &&
+				node[child].getKey() > node[child + 1].getKey()) {
+				child++;
+			}
+
+			// 마지막 노드가 자식보다 작거나 같으면 제자리
+			if (last.getKey() <= node[child].getKey()) {
+				break;
+			}
+
+			node[parent] = node[child];
+			parent = child;
+			child *= 2;
+		}
+
+		node[parent] = last;
+
+		return item;
+	}
+};
+
+class WGraphMST : public WGraph {
+public:
+	void Kruskal() { // kruskal의 최소 비용 신장 트리 프로그램
+		MinHeap heap;
+		for (int i = 0; i < size - 1; i++)
+			for (int j = i + 1; j < size; j++)
+				if (hasEdge(i, j))
+					heap.insert(getEdge(i, j), i, j); // 모든 간선 삽입
+		VertexSets set(size); // size개의 집합을 만듦
+		int edgeAccepted = 0; // 선택된 간선의 수
+		while (edgeAccepted < size - 1) { // 간선의 수 < (size-1)
+			HeapNode& e = heap.remove();
+			int uset = set.findSet(e.getV1());
+			int vset = set.findSet(e.getV2());
+			if (uset != vset) {
+				printf("간선 추가 : %c - %c (비용:%d)\n",
+					getVertex(e.getV1()), getVertex(e.getV2()), e.getKey());
+				set.unionSets(uset, vset); // 두개의 집합을 합함.
+				edgeAccepted++;
+			}
+		}
 	}
 };
--- Lec10_Graph/Lec10_Graph/main.cpp
+++ Lec10_Graph/Lec10_Graph/main.cpp
@@ -2,42 +2,55 @@
 
 int main()
 {
-	AdjMatGraph g; // 새로운 그래프 객체 생성
-	for (int i = 0; i < 4; i++)
-		g.insertVertex('A' + i); // 정점 삽입: 'A' 'B', ...
-	g.insertEdge(0, 1); // 간선 삽입
-	g.insertEdge(0, 3);
-	g.insertEdge(1, 2);
-	g.insertEdge(1, 3);
-	g.insertEdge(2, 3);
-	printf("인접 행렬로 표현한 그래프\n");
-	g.display();
+	//AdjMatGraph g; // 새로운 그래프 객체 생성
+	//for (int i = 0; i < 4; i++)
+	//	g.insertVertex('A' + i); // 정점 삽입: 'A' 'B', ...
+	//g.insertEdge(0, 1); // 간선 삽입
+	//g.insertEdge(0, 3);
+	//g.insertEdge(1, 2);
+	//g.insertEdge(1, 3);
+	//g.insertEdge(2, 3);
+	//printf("인접 행렬로 표현한 그래프\n");
+	//g.display();
 
-	FILE* fp;
-	fopen_s(&fp, "../text_out.txt", "w");
-	g.display(fp);
-	fclose(fp);
+	//FILE* fp;
+	//fopen_s(&fp, "../text_out.txt", "w");
+	////g.display(fp);
+	//fclose(fp);
 
-	SrchAMGraph g2; // DFS 탐색기능이 있는 그래프 객체 생성
+	//SrchAMGraph g2; // DFS 탐색기능이 있는 그래프 객체 생성
 	//g.load("graph.txt"); // 파일 "graph.txt"로부터 g를 설정함
-	for (int i = 0; i < 8; i++)
-		g2.insertVertex('A' + i); // 정점 삽입: 'A' 'B', ...
-	g2.insertEdge(0, 1); // 간선 삽입
-	g2.insertEdge(0, 3);
-	g2.insertEdge(1, 2);
-	g2.insertEdge(1, 3);
-	g2.insertEdge(2, 3);
-	g2.insertEdge(2, 3);
-	g2.insertEdge(4, 5);
-	g2.insertEdge(3, 6);
-	g2.insertEdge(5, 7);
-	g2.insertEdge(0, 5);
-	printf("그래프(graph.txt)\n");
-	g2.display(); // 그래프를 화면에 출력
-	printf("DFS ==> ");
-	g2.resetVisited(); // 모든 정점을 방문하지 않았다고 하고
-	g2.DFS(0); // 0번째 정점(A)에서 깊이 우선 탐색 시작
-	printf("\n");
+	//for (int i = 0; i < 8; i++)
+	//	g2.insertVertex('A' + i); // 정점 삽입: 'A' 'B', ...
+	//g2.insertEdge(0, 1); // 간선 삽입
+	//g2.insertEdge(0, 3);
+	//g2.insertEdge(1, 2);
+	//g2.insertEdge(1, 3);
+	//g2.insertEdge(2, 3);
+	//g2.insertEdge(2, 3);
+	//g2.insertEdge(4, 5);
+	//g2.insertEdge(3, 6);
+	//g2.insertEdge(5, 7);
+	//g2.insertEdge(0, 5);
+	//printf("그래프(graph.txt)\n");
+	//g2.display(); // 그래프를 화면에 출력
+	//printf("DFS ==> ");
+	//g2.resetVisited(); // 모든 정점을 방문하지 않았다고 하고
+	//g2.DFS(0); // 0번째 정점(A)에서 깊이 우선 탐색 시작
+	//printf("\n");
+
+	//WGraph wg;
+	//wg.load("../graph.txt");
+	//wg.display();
+
+	WGraphMST g3;
+	g3.load("graph.txt");
+	cout << "입력 그래프: graph.txt" << endl;
+	g3.display();
+	cout << "MST By Kruskal's Algorithm" << endl;
+	g3.Kruskal();
+
+	return 0;
 
 	return 0;
 }
